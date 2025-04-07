540a728cb7d755476e4810ba033339936ae3fb4c
r15uArch <rk5718158@gmail.com>
2025-04-08 00:43:42 +0530

    rps-ui

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-let choices = ["rock", "scissors", "papers"]; //array of choices
+let choices = ["rock", "scissors", "paper"]; //array of choices
 
 function getComputerChoice(){
     let val = Math.floor(Math.random() * 3);
     return choices[val];
 }
 
-function getHumanChoice(){
-    return parseInt(prompt("Enter 1 for rock, 2 for scissors, 3 for papers: "));
-}
+// function getHumanChoice(){
+//     return parseInt(prompt("Enter 1 for rock, 2 for scissors, 3 for papers: "));
+// }
 
 let humanScore = 0;
 let computerScore = 0;
 
 
+const div = document.querySelector(".results");
+div.textContent = `Human Score: ${humanScore} Computer Score: ${computerScore}`;
+const winner = document.querySelector(".winner");
 
-function playGame(){
-    function playRound(humanChoice, computerChoice){
-        if(humanChoice == computerChoice){
-            alert(`It's a tie. Both picked ${humanChoice}`);
-        }
-        else if((humanChoice == 'rock' && computerChoice == 'scissors')||
-                (humanChoice == 'paper' && computerChoice == 'rock') ||
-                (humanChoice == 'scissors' && computerChoice == 'paper')){
-                    alert(`Human wins!!! ${humanChoice} beats ${computerChoice}`);
-                    humanScore++;
-                }
-        else{
-            alert(`Computer Wins!!! ${computerChoice} beats ${humanChoice}`);
-            computerScore++;
-        }
+function playRound(humanChoice, computerChoice){
+    if(humanChoice == computerChoice){
+        // alert(`It's a tie. Both picked ${humanChoice}`);
     }
-    
+    else if((humanChoice == 'rock' && computerChoice == 'scissors')||
+            (humanChoice == 'paper' && computerChoice == 'rock') ||
+            (humanChoice == 'scissors' && computerChoice == 'paper')){
+                // alert(`Human wins!!! ${humanChoice} beats ${computerChoice}`);
+                humanScore++;
+            }
+    else{
+        // alert(`Computer Wins!!! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+    div.textContent = `Human Score: ${humanScore} Computer Score: ${computerScore}`;
+    if(humanScore == 5)
+        winner.textContent = `Human Wins!!!`;
+    else if(computerScore == 5)
+        winner.textContent = `Computer Wins!!!`;
+
+}
+
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        let human = e.target.id;
+        // console.log(e.target);
+        // console.log(e.target.id);
+        let computer = getComputerChoice();
+        playRound(human, computer);
+    });
+});
+
+
+
+
+
+/*
     for(let i = 0; i < 5;){
         let computer = getComputerChoice();
         let number = getHumanChoice();
@@ -42,10 +67,5 @@ function playGame(){
         let human = choices[number - 1];
         playRound(human, computer);
         i++;
-    }
-        
-}
-
-playGame();
-alert(`Overall final score: \nComputer: ${computerScore} \nHuman: ${humanScore}`);
+    }*/
 
